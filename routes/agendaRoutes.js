1c931433ed0aefceec9f85950b95e4c6888a5f1b
Marcos Santos Martirio <mmartirio@github.com>
2026-01-22 21:01:26 -0300

    feat(agenda): adiciona rotas para salvar indisponibilidade e encerramento antecipado

--- routes/agendaRoutes.js
+++ routes/agendaRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const agendaController = require('../controllers/agendaController');
+
+// Salvar indisponibilidade
+router.post('/indisponibilidade', agendaController.saveIndisponibilidade);
+
+// Salvar encerramento antecipado
+router.post('/encerramento-antecipado', agendaController.saveEncerramentoAntecipado);
+
+module.exports = router;
 
